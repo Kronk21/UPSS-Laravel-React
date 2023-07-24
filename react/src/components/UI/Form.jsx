2c1737548912131e4bced12f94d40228359bf9da
Kronk21 <CesarAxel001@gmail.com>
2023-07-24 15:26:01 -0600

    Add margin top to the contact form

--- react/src/components/UI/Form.jsx
+++ react/src/components/UI/Form.jsx
@@ -1,38 +1,31 @@
 const Form = function () {
     return (
-        <>
-            <section id="contacto" className="seccion-formulario">
-                <div className="contenedor">
-                    <div className="formulario__contenido">
-                        <h3>Envianos un mensaje</h3>
-                        <form action="#contacto" className="formulario">
-                            <div className="formulario__control">
-                                <label htmlFor="correo">
-                                    Tu correo electronico:
-                                </label>
-                                <input type="email" name="correo" id="correo" />
-                            </div>
-                            <div className="formulario__control">
-                                <label htmlFor="mensaje">
-                                    Escribe un mensaje:
-                                </label>
-                                <textarea
-                                    name="mensaje"
-                                    id="mensaje"
-                                ></textarea>
-                            </div>
-                            <div className="formulario__control"></div>
-                            <input
-                                type="submit"
-                                name="enviar_mensaje"
-                                className="btn btn-secundario"
-                                value="Enviar"
-                            />
-                        </form>
-                    </div>
+        <section id="contacto" className="seccion-formulario">
+            <div className="contenedor">
+                <div className="formulario__contenido">
+                    <h3>Envianos un mensaje</h3>
+                    <form action="#contacto" className="formulario">
+                        <div className="formulario__control">
+                            <label htmlFor="correo">
+                                Tu correo electronico:
+                            </label>
+                            <input type="email" name="correo" id="correo" />
+                        </div>
+                        <div className="formulario__control">
+                            <label htmlFor="mensaje">Escribe un mensaje:</label>
+                            <textarea name="mensaje" id="mensaje"></textarea>
+                        </div>
+                        <div className="formulario__control"></div>
+                        <input
+                            type="submit"
+                            name="enviar_mensaje"
+                            className="btn btn-secundario"
+                            value="Enviar"
+                        />
+                    </form>
                 </div>
-            </section>
-        </>
+            </div>
+        </section>
     );
 };
 
