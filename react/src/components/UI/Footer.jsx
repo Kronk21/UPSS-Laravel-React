6373b1630ad77e05ee6b14cef14ed92aa94e7ceb
Kronk21 <CesarAxel001@gmail.com>
2023-07-24 15:32:42 -0600

    Fix some react html quirks on the Footer component

--- react/src/components/UI/Footer.jsx
+++ react/src/components/UI/Footer.jsx
@@ -3,43 +3,45 @@ import colorLogo from "../../assets/Logo-Color.png";
 const Footer = function () {
     return (
         <footer>
-            <div class="contenedor footer__grid">
-                <div class="footer__col">
+            <div className="contenedor footer__grid">
+                <div className="footer__col">
                     <img src={colorLogo} alt="UPSS Wireless logo" />
-                    <p class="footer__aviso">
+                    <p className="footer__aviso">
                         Copyright © 2022 por UPSS Wireless, Todos los derechos
                         reservados.
                     </p>
                 </div>
-                <div class="footer__col">
+                <div className="footer__col">
                     <ul>
                         <li>
-                            <i class="fa-solid fa-location-dot footer__icono"></i>
-                            <p class="footer__info">
+                            <i className="fa-solid fa-location-dot footer__icono"></i>
+                            <p className="footer__info">
                                 Av insurgentes #97 col. el Carmen. Ixmiquilpan,
                                 Hgo.
                             </p>
                         </li>
                         <li>
-                            <i class="fa-solid fa-phone footer__icono"></i>
-                            <p class="footer__info">(+759) 759 723 0294</p>
+                            <i className="fa-solid fa-phone footer__icono"></i>
+                            <p className="footer__info">(+759) 759 723 0294</p>
                         </li>
                         <li>
-                            <i class="fa-solid fa-envelope footer__icono"></i>
-                            <p class="footer__info">uppswireless@gmail.com</p>
+                            <i className="fa-solid fa-envelope footer__icono"></i>
+                            <p className="footer__info">
+                                uppswireless@gmail.com
+                            </p>
                         </li>
                     </ul>
                 </div>
-                <div class="footer__col">
+                <div className="footer__col">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14950.910467076743!2d-99.2138024!3d20.4763777!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x300107126dd010b8!2sUpss%20Wireless!5e0!3m2!1ses-419!2smx!4v1663429970414!5m2!1ses-419!2smx"
                         width="600"
                         height="450"
                         // style="border:0;"
-                        allowfullscreen=""
+                        allowFullScreen=""
                         loading="lazy"
-                        referrerpolicy="no-referrer-when-downgrade"
-                        class="footer__mapa"
+                        referrerPolicy="no-referrer-when-downgrade"
+                        className="footer__mapa"
                     ></iframe>
                 </div>
             </div>
