f5791f86ade415bfb714f9a879971fa94ef66d76
Kronk21 <CesarAxel001@gmail.com>
2023-07-24 15:11:47 -0600

    Add footer

---
+++ react/src/components/UI/Footer.jsx
@@ -0,0 +1,51 @@
+const Footer = function () {
+    return (
+        <footer>
+            <div class="contenedor footer__grid">
+                <div class="footer__col">
+                    <img
+                        src="src/img/Logo-Color.png"
+                        alt="UPSS Wireless logo"
+                    />
+                    <p class="footer__aviso">
+                        Copyright © 2022 por UPSS Wireless, Todos los derechos
+                        reservados.
+                    </p>
+                </div>
+                <div class="footer__col">
+                    <ul>
+                        <li>
+                            <i class="fa-solid fa-location-dot footer__icono"></i>
+                            <p class="footer__info">
+                                Av insurgentes #97 col. el Carmen. Ixmiquilpan,
+                                Hgo.
+                            </p>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-phone footer__icono"></i>
+                            <p class="footer__info">(+759) 759 723 0294</p>
+                        </li>
+                        <li>
+                            <i class="fa-solid fa-envelope footer__icono"></i>
+                            <p class="footer__info">uppswireless@gmail.com</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer__col">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14950.910467076743!2d-99.2138024!3d20.4763777!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x300107126dd010b8!2sUpss%20Wireless!5e0!3m2!1ses-419!2smx!4v1663429970414!5m2!1ses-419!2smx"
+                        width="600"
+                        height="450"
+                        // style="border:0;"
+                        allowfullscreen=""
+                        loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"
+                        class="footer__mapa"
+                    ></iframe>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- react/src/pages/DefaultLayout.jsx
+++ react/src/pages/DefaultLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 
+import Footer from "../components/UI/Footer";
+
 const DefaultLayout = function () {
     return (
         <>
             <h1>Default layout</h1>
 
             <Outlet />
+
+            <Footer />
         </>
     );
 };
