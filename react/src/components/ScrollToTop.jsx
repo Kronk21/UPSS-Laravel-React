64430b05c7639e78ba336feb647d727e730b8c37
Kronk21 <CesarAxel001@gmail.com>
2023-07-26 17:27:00 -0600

    Fix scrollToTop component

--- react/src/components/ScrollToTop.jsx
+++ react/src/components/ScrollToTop.jsx
@@ -2,17 +2,17 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = function () {
-    const { pathUrl } = useLocation();
+    const { pathname } = useLocation();
 
     useEffect(
         function () {
             document.documentElement.scrollTo({
                 top: 0,
                 left: 0,
-                behavior: "instant", // Optional if you want to skip the scrolling animation
+                // behavior: "instant", // Optional if you want to skip the scrolling animation
             });
         },
-        [pathUrl]
+        [pathname]
     );
 
     return null;
