913c121e39b899ddf7a3f9d41ee980d77bcaac86
Kronk21 <CesarAxel001@gmail.com>
2023-07-26 17:15:41 -0600

    Add basic api calls

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +19,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource("category", CategoryController::class);
+
+Route::prefix("/products")->group(function () {
+    Route::get("/", function () {
+        return ProductResource::collection(Product::all());
+    })->name("products.index");
+
+    Route::get("/{product}", function (Product $product) {
+        return new ProductResource($product);
+    })->name("products.show");
 });
