9cb891e31378f6a29988547fc365d4bec494d2f7
Kronk21 <CesarAxel001@gmail.com>
2023-07-26 18:35:50 -0600

    Add fetching to the CategoryScroll component and fill the product cards with real data

--- react/src/components/Products/ProductCard.jsx
+++ react/src/components/Products/ProductCard.jsx
@@ -2,18 +2,20 @@ import { Link } from "react-router-dom";
 
 import img from "../../assets/img/61dBxsqt5pL._AC_SL1000_.jpg";
 
-const ProductCard = function () {
+const ProductCard = function ({ product }) {
     return (
-        <Link to="/products/1" className="producto">
+        <Link to={`/products/${product.id}`} className="producto">
             <div className="producto__img">
-                <img src={img} alt="Fotografia de" />
+                <img src={img} alt={`Fotografia de ${product.title}`} />
             </div>
             <div className="producto__info">
-                <p className="producto__nombre">
-                    STF Audífonos De Diadema Inalámbricos Aurum, Conexión 5.0,
-                    Micrófono Incorporado{" "}
+                <p className="producto__nombre">{product.title}</p>
+                <p className="producto__precio">
+                    {new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "USD",
+                    }).format(product.price)}
                 </p>
-                <p className="producto__precio">$280.00</p>
             </div>
         </Link>
     );
