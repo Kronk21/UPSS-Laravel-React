57e6424909a624ab4e60ce986795c1eedf47a799
Kronk21 <CesarAxel001@gmail.com>
2023-07-29 17:45:29 -0600

    Add the images seeding to the DatabaseSeeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Category;
+use App\Models\Image;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 
@@ -15,18 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create categories
         foreach (Category::$categories as $category) {
             Category::factory()->create([
                 "title" => $category,
             ]);
         }
 
+        // Create products
         $categories = Category::all();
         foreach ($categories as $category) {
             $number = random_int(10, 30);
 
             Product::factory($number)->create([
                 "category_id" => $category->id,
+            ]);
+        }
+
+        // Create images
+        $products = Product::all();
+        foreach ($products as $product) {
+            Image::factory(4)->create([
+                "product_id" => $product->id,
             ]);
         }
 
